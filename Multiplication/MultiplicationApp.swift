2ed20cb18139176a2582fbbf6c0cf5e63de56451
JakubCzerwiec <jacob.czerwiec@gmail.com>
2024-10-08 00:46:13 +0200

    Initial Commit

---
+++ Multiplication/MultiplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationApp.swift
+//  Multiplication
+//
+//  Created by Mój Maczek on 08/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
